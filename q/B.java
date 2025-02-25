a60fb97de1b59c408ca8438f3df2dc9364a8c9a6
Anshuman9016 <Anshuman.119016@stu.upes.ac.in>
2025-02-26 01:42:28 +0530

    B.java

--- q/B.java
+++ q/B.java
@@ -9,15 +9,12 @@ public class B {
         // Accessing public method
         a.publicMethod();
 
-        // Accessing protected method (will cause an error)
-        // a.protectedMethod();
-
-        // Accessing default method (will cause an error)
-        // a.defaultMethod();
-
-        // Accessing private method (not accessible)
-        // a.privateMethod();
-
+        // Accessing protected method 
+       
+        // Accessing default method 
+       
+        // Accessing private method 
+       
         // Accessing private method through a public method within the same class
         a.accessPrivateMethod();
     }
