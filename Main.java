bc313ed29543616ec48d14b247ac2c851dfbc889
Anshuman9016 <Anshuman.119016@stu.upes.ac.in>
2025-02-26 01:51:47 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,9 @@
-
-
-
-
-
-
-
 public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle();
 
-       
-
         // Calculate and print the area of a circle with radius 5
         circle.calculateArea(5);
-
-        // Uncommenting the following line will cause a compilation error
-        // because PI is a final variable and cannot be modified.
         // circle.PI = 3.14;
     }
 }
